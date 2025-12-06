800e6992f81c9677c0f70028542e2094c8cd8f07
SvenWeinert <sven@sihensen.de>
2025-12-06 19:31:28 +0100

    create quiz in right folder

---
+++ src/app/component/create-quiz/create-quiz-service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CreateQuizService {
+
+  _url = "http://localhost:8080/quizlounge/api/quiz/create";
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+
+  createQuiz(title:string, description:string, category:string) : Observable<any> {
+    return this.httpClient.post(this._url, {title, description, category}, {
+      withCredentials: true
+    });
+  }
+
+}
