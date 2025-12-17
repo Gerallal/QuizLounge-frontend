160cd271e1166cd533178a9fe5467da7f3e631e4
alexavm268 <alexa.vonmersewsky@gmx.de>
2025-12-17 13:38:22 +0100

    -

---
+++ src/app/component/create-quiz/create-quiz.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-quiz',
+  imports: [],
+  templateUrl: './create-quiz.html',
+  styleUrl: './create-quiz.css',
+})
+export class CreateQuiz {
+
+}
